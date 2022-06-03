31296439aa4f741573e06b83a6b519d92433487d
Alina <pisarenko.alina@gmail.com>
2022-06-03 13:44:15 -0400

    finished css

--- src/components/App.js
+++ src/components/App.js
@@ -15,53 +15,52 @@ import Header from "./Header";
 import HomeHeader from "./HomeHeader";
 import ManageAccount from "./ManageAccount";
 import Workout from "./Workout";
-import { login, signup } from './crud'
+import { login, signup } from "./crud";
 
 const reducer = (user, action) => {
   switch (action.type) {
-    case 'login':
+    case "login":
       localStorage.setItem("user", JSON.stringify(action.payload.user));
-      return action.payload.user
-    case 'logout':
+      return action.payload.user;
+    case "logout":
       localStorage.clear();
-      return {}
-    case 'delete':
+      return {};
+    case "delete":
       localStorage.clear();
-      return {}
+      return {};
   }
-}
+};
 
 function App() {
   const [user, dispatch] = useReducer(reducer, {}, () => {
-    const localUser = localStorage.getItem("user")
-    return localUser ? JSON.parse(localUser) : {}
+    const localUser = localStorage.getItem("user");
+    return localUser ? JSON.parse(localUser) : {};
   });
 
-  const [error, setError] = useState('')
+  const [error, setError] = useState("");
 
   const history = useHistory();
   let { pathname } = useLocation();
 
   const handleLogin = (userInfo) => {
     login(userInfo).then((data) => {
-        if (data) {
-          setError('')
-          dispatch({ type: 'login', payload: {user: data }})
-          history.push(`/user/${data.id}`);
-        }
-        else {
-          setError('Oops I did it again')
-        }
-      })
-  }
+      if (data) {
+        setError("");
+        dispatch({ type: "login", payload: { user: data } });
+        history.push(`/user/${data.id}`);
+      } else {
+        setError("Oops I did it again");
+      }
+    });
+  };
 
   const handleSignUp = (newUser) => {
     signup(newUser).then((data) => {
-        if (data) {
-          dispatch({ type: 'login', payload: {user: data }})
-          history.push(`/user/${data.id}`);
-        }
-      });
+      if (data) {
+        dispatch({ type: "login", payload: { user: data } });
+        history.push(`/user/${data.id}`);
+      }
+    });
   };
 
   return (
@@ -73,26 +72,27 @@ function App() {
       )}
       <Switch>
         <Route exact path="/">
-          {Object.keys(user).length !== 0 ? 
-            <Redirect to={"/user/" + user.id} /> : <HomePage /> 
-          }
+          {Object.keys(user).length !== 0 ? (
+            <Redirect to={"/user/" + user.id} />
+          ) : (
+            <HomePage />
+          )}
         </Route>
         <Route exact path="/login">
-          <Login
-            error={error}
-            handleLogin={handleLogin}
-          />
+          <Login error={error} handleLogin={handleLogin} />
         </Route>
         <Route exact path="/signup">
-          <Signup
-            handleSignUp={handleSignUp}
-          />
+          <Signup handleSignUp={handleSignUp} />
         </Route>
         <Route path="/user/:id">
-          {Object.keys(user).length !== 0 ? <UserInfo id={user.id} /> : <Redirect to="/" />}
+          {Object.keys(user).length !== 0 ? (
+            <UserInfo id={user.id} />
+          ) : (
+            <Redirect to="/" />
+          )}
         </Route>
         <Route exact path="/update">
-          <ManageAccount id={user.id} dispatch={dispatch}/>
+          <ManageAccount id={user.id} dispatch={dispatch} />
         </Route>
         <Route exact path="/workouts">
           <Workout />
